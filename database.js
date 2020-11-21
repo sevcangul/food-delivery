1ac31b9a1d4f580e018af832f33e21b31410b1e0
Sevcan <sevcansuperpeer@gmail.com>
2020-11-21 22:49:39 +0300

    Add save, load, insert and remove functionalities to the database

--- database.js
+++ database.js
@@ -1,13 +1,26 @@
 const fs = require('fs')
+const flatted = require('flatted')
+const { finished } = require('stream')
 
 const save = (filename, objects) => {
-  fs.writeFileSync(`./${filename}.json`, JSON.stringify(objects, null, 2))
+  fs.writeFileSync(`./${filename}.json`, flatted.stringify(objects, null, 2))
 }
 
 const load = (filename) => {
   const file = fs.readFileSync(`./${filename}.json`, 'utf-8')
-  return JSON.parse(file)
+  return flatted.parse(file)
 }
 
+const insert = (filename, object) => {
+  const objects = load(filename)
+  save(filename, objects.concat(object)) // = add object to the end of the object, return new array and save it
+}
 
-module.exports = { save, load }
+const remove = (filename, index) => {
+  const objects = load(filename)
+
+  objects.splice(index, 1)
+  save(filename, objects)
+}
+
+module.exports = { save, load, insert, remove }
--- index.js
+++ index.js
@@ -4,35 +4,50 @@ const Customer = require('./customer')
 const Restaurant = require('./restaurant')
 
 const sevcan = new Customer('Sevcan', 'hamburger', 'Urla')
+const gizem = new Customer('Gizem', 'cake', 'Karşıyaka')
 const bigchefs = new Restaurant('Big Chefs', 'Alsancak')
 
 const order1 = sevcan.giveAnOrder(bigchefs, 'hamburger', 'Urla')
 const order2 = sevcan.giveAnOrder(bigchefs, 'pizza', 'Karşıyaka')
 const order3 = sevcan.giveAnOrder(bigchefs, 'pasta', 'Alsancak')
 const order4 = sevcan.giveAnOrder(bigchefs, 'cheeseburger', 'Bostanlı')
+const order5 = gizem.giveAnOrder(bigchefs, 'cake', 'Karşıyaka')
 
 function printOrder(order) {
   console.log(`${colors.blue(order.customer.name)} wants to order ${colors.bgBlue.white(order.food)} from ${colors.green(order.restaurant.brand)} to ${colors.bgGreen.white(order.origin)}`)
 }
 
 function printOrderHistory(customer) {
-  sevcan.orders.forEach(printOrder)
+  customer.orders.forEach(printOrder)
 }
 
-db.save('customers', [{name: 'Sevcan', food: 'hamburger', location: 'Urla'}])
+// db.save('customers', [sevcan, gizem])
+// // // db.save('customers', [{name: 'Sevcan', food: 'hamburger', location: 'Urla'}])
 // db.save('restaurants', [bigchefs])
 
-const customers = db.load('customers')
-console.log(customers)
+// const dilek = new Customer('Dilek', 'cheeseburger', 'Güzelbahçe')
+// db.insert('customers', dilek)
+db.remove('customers', 2)
 
-// customers.forEach(printOrderHistory)
+const customers = db.load('customers')
+
+customers.forEach(p => console.log(p.name))
+
+
+
+
+
+
+
+
+
 
 // printOrderHistory(sevcan)
 
-console.log(`${sevcan.name} has ${sevcan.orders.length} order(s).`)
-console.log(sevcan.orders[0].restaurant.location)
-console.log(sevcan.orders[0].origin)
-console.log(sevcan.orders[1].food)
+// console.log(`${sevcan.name} has ${sevcan.orders.length} order(s).`)
+// console.log(sevcan.orders[0].restaurant.location)
+// console.log(sevcan.orders[0].origin)
+// console.log(sevcan.orders[1].food)
 
 
 
