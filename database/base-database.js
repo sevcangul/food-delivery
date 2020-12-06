8f0293960b2fd28c1ca90288b9994a2e0e8050ba
Sevcan <ssevcangul@gmail.com>
2020-12-06 22:32:26 +0300

    Change the base database

--- database/base-database.js
+++ database/base-database.js
@@ -7,44 +7,47 @@ class BaseDatabase {
     this.filename = model.name.toLowerCase()
   }
 
-save(objects) {
-  fs.writeFileSync(`./${this.filename}.json`, flatted.stringify(objects, null, 2))
+  save(objects) {
+    fs.writeFileSync(`${__dirname}/${this.filename}.json`, flatted.stringify(objects, null, 2))
   }
 
-load() {
-  const file = fs.readFileSync(`./${this.filename}.json`, 'utf-8')
-  const objects = flatted.parse(file)
+  load() {
+    const file = fs.readFileSync(`${__dirname}/${this.filename}.json`, 'utf8')
+    const objects = flatted.parse(file)
 
-  return objects.map(this.model.create)
+    return objects.map(this.model.create)
+  }
+
+  insert(object) {
+    const objects = this.load()
+    this.save(objects.concat(object)) // = add object to the end of the object, return new array and save it
+  }
+
+  remove(index) {
+    const objects = this.load()
+
+    objects.splice(index, 1)
+    this.save(objects)
+  }
+
+  update(object) {
+    const objects = this.load()
+
+    const index = objects.findIndex(o => o.id == object.id)
+
+    if (index == -1) throw new Error(`Cannot find ${this.model.name} instance with id ${object.id}`)
+
+    objects.splice(index, 1, object)
+    this.save(objects)
+  }
+  find(id) {
+    return this.load().find(o => o.id == id)
+  }
+
+  findBy(property, value) {
+    return this.load().find(o => o[property] == value)
+  }
+
 }
 
-insert(object) {
-  const objects = this.load()
-  this.save(objects.concat(object)) // = add object to the end of the object, return new array and save it
-  }
-
-remove(index) {
-  const objects = this.load()
-
-  objects.splice(index, 1)
-  this.save(objects)
-  }
-
-update(object) {
-  const objects = this.load()
-
-  const index = objects.findIndex(o => o.id == object.id)
-
-  if(index == -1) throw new Error(`Cannot find ${this.model.name} instance with id ${object.id}`)
-
-  objects.splice(index, 1, object)
-  this.save(objects)
-  }
-
-findBy(property, value) {
-  return this.load().find(o => o[property] == value)
-  }
-
-}
-
- module.exports = BaseDatabase
+module.exports = BaseDatabase
