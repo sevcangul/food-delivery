0f654f02930c6b5c636bc50c049a677daaf18e4e
Sevcan <ssevcangul@gmail.com>
2020-12-06 22:54:02 +0300

    Move the customer.js to the models folder

---
+++ models/customer.js
@@ -0,0 +1,26 @@
+const Order = require('./order')
+const uuid = require('uuid')
+
+class Customer {
+  constructor(id = uuid.v4(), name, food, location, orders = []) {
+    this.id = id
+
+    this.name = name
+    this.food = food
+    this.location = location
+    this.orders = orders
+  }
+
+  giveAnOrder(restaurant, food, origin) {
+    const order = new Order(this, restaurant, food, origin)
+    this.orders.push(order)
+
+    return order
+  }
+
+  static create({id, name, food, location, orders}) {
+    return new Customer(id, name, food, location, orders)
+  }
+}
+
+module.exports = Customer
