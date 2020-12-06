30ab173fcd50bcaef5d20093abf087a0562dc9cd
Sevcan <ssevcangul@gmail.com>
2020-12-06 22:39:42 +0300

    Change the restaurant database

--- database/restaurant-database.js
+++ database/restaurant-database.js
@@ -1,15 +1,13 @@
 const BaseDatabase = require('./base-database')
-const Restaurant = require('../restaurant')
+const Restaurant = require('../models/restaurant')
 
 class RestaurantDatabase extends BaseDatabase {
   findByRestaurantName(brand) {
     return this.findBy('brand', brand)
   }
-
   findByRestaurantLocation(location) {
     return this.findBy('location', location)
   }
-
 }
 
 module.exports = new RestaurantDatabase(Restaurant)
