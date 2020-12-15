f99b57ed1a15f8b02354dbfa98afc35b05a78086
Sevcan <ssevcangul@gmail.com>
2020-12-15 22:37:39 +0300

    Remove orderId and origin properties from Order class

--- models/order.js
+++ models/order.js
@@ -4,10 +4,8 @@ class Order {
   constructor(customer, restaurant, food, id = uuid.v4()) {
 
     this.customer = customer
-    // this.orderId = orderId
     this.restaurant = restaurant
     this.food = food
-    // this.origin = origin // customer.location?
     this.id = id
   }
 }
